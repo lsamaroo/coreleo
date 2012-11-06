23fdbeb82e9c50dece0af2a99dfdb0bf2d7a3626
leon <orrinsamaroo@yahoo.com>
2012-11-05 23:19:56 -0500

    Add new util method compare

--- coreleo_new/src/main/java/com/coreleo/web/login/AbstractCheckIfLoggedInFilter.java
+++ coreleo_new/src/main/java/com/coreleo/web/login/AbstractCheckIfLoggedInFilter.java
@@ -3,9 +3,14 @@
  */
 package com.coreleo.web.login;
 
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import com.coreleo.servlet.filter.AbstractFilter;
 import com.coreleo.util.WebUtil;
@@ -14,45 +19,49 @@ import com.coreleo.util.WebUtil;
  * @author Leon Samaroo
  * 
  */
-public abstract class AbstractCheckIfLoggedInFilter extends AbstractFilter {
-	protected int INVALID_REQUEST  = -1;
-	protected int INVALID_SESSION  = -2;
-	protected int NOT_LOGGED_IN  = -3;
+public abstract class AbstractCheckIfLoggedInFilter extends AbstractFilter
+{
+	protected int INVALID_REQUEST = -1;
+	protected int INVALID_SESSION = -2;
+	protected int NOT_LOGGED_IN = -3;
 
-
-	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException
+	{
 		final HttpServletRequest request = (HttpServletRequest) req;
 		final HttpServletResponse response = (HttpServletResponse) res;
 
 		// exempt check
-		if (isRequestedPageExemptFromCheck(request)) {
+		if (isRequestedPageExemptFromCheck(request))
+		{
 			chain.doFilter(request, response);
 			return;
 		}
-		
-		
-		if (request == null ) {
-			WebUtil.redirect(request, response, getErrorPage( INVALID_REQUEST ));
-			return;			
+
+		if (request == null)
+		{
+			WebUtil.redirect(request, response, getErrorPage(INVALID_REQUEST));
+			return;
 		}
 
 		// check session
-		if ( request.getSession(false) == null ) {
-			WebUtil.redirect(request, response, getErrorPage( INVALID_SESSION ));
-			return;			
+		if (request.getSession(false) == null)
+		{
+			WebUtil.redirect(request, response, getErrorPage(INVALID_SESSION));
+			return;
 		}
 
-		if (!LoginHelper.isUserLoggedIn(request)) {
-			WebUtil.redirect(request, response, getErrorPage( NOT_LOGGED_IN ));
+		if (!LoginHelper.isUserLoggedIn(request))
+		{
+			WebUtil.redirect(request, response, getErrorPage(NOT_LOGGED_IN));
 			return;
-		} 
-		
+		}
+
 		chain.doFilter(request, response);
-		return;
 	}
 
 	protected abstract boolean isRequestedPageExemptFromCheck(HttpServletRequest request);
 
-	protected abstract String getErrorPage( int errorCode );
+	protected abstract String getErrorPage(int errorCode);
 
 }
