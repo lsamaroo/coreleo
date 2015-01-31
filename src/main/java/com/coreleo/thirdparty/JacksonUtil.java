34b54ccb58e93a359f80782b826069f865d5b151
Leon Samaroo <orrinsamaroo@yahoo.com>
2015-01-31 02:42:28 -0500

    Add method to convert json string to map

--- src/main/java/com/coreleo/thirdparty/JacksonUtil.java
+++ src/main/java/com/coreleo/thirdparty/JacksonUtil.java
@@ -1,19 +1,38 @@
 package com.coreleo.thirdparty;
 
+import java.util.HashMap;
+import java.util.Map;
 
 import com.coreleo.util.LogUtil;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JacksonUtil {
 
 	public static final String toJsonString(final Object obj) {
-		try {
+		try
+		{
 			return new ObjectMapper().writeValueAsString(obj);
-		} 
-		catch (final Exception e) {
+		}
+		catch (final Exception e)
+		{
 			LogUtil.error(e);
 			return null;
 		}
 	}
 
+	public static final Map<String, String> fromJson(final String json) {
+		try
+		{
+			final ObjectMapper mapper = new ObjectMapper();
+			return mapper.readValue(json, new TypeReference<HashMap<String, String>>() {
+			});
+		}
+		catch (final Exception e)
+		{
+			LogUtil.error(e);
+			return null;
+		}
+
+	}
 }
