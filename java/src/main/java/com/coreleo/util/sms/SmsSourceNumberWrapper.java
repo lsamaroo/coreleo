8d1ebe152b33468d9c086dfc748ea6b56bd6cc16
Leon Samaroo <orrinsamaroo@gmail.com>
2023-01-01 19:07:05 -0500

    Removed suppress warning from class

--- java/src/main/java/com/coreleo/util/sms/SmsSourceNumberWrapper.java
+++ java/src/main/java/com/coreleo/util/sms/SmsSourceNumberWrapper.java
@@ -1,31 +1,30 @@
 package com.coreleo.util.sms;
 
-@SuppressWarnings({ "rawtypes", "unchecked" })
 public class SmsSourceNumberWrapper implements SourceNumber {
-	private final String sourceNumber;
-	private final SmsSourceNumberPool pool;
+    private final String sourceNumber;
+    private final SmsSourceNumberPool pool;
 
-	public SmsSourceNumberWrapper(final SmsSourceNumberPool pool, final String sourceNumber) {
-		this.sourceNumber = sourceNumber;
-		this.pool = pool;
-	}
+    public SmsSourceNumberWrapper(final SmsSourceNumberPool pool, final String sourceNumber) {
+        this.sourceNumber = sourceNumber;
+        this.pool = pool;
+    }
 
-	// -------------------------------------------------------------------------------------------
-	// Implementation of interface SourceNumber
-	// -------------------------------------------------------------------------------------------
+    // -------------------------------------------------------------------------------------------
+    // Implementation of interface SourceNumber
+    // -------------------------------------------------------------------------------------------
 
-	@Override
-	public void close() {
-		// pool.checkIn(this);
-	}
+    @Override
+    public void close() {
+        // pool.checkIn(this);
+    }
 
-	@Override
-	public boolean isValid() {
-		return true;
-	}
+    @Override
+    public boolean isValid() {
+        return true;
+    }
 
-	public String getSourceNumber() {
-		return sourceNumber;
-	}
+    public String getSourceNumber() {
+        return sourceNumber;
+    }
 
 }
